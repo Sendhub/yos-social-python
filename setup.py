ea04b484684cb399b0b19815a8cff04800fa7cbc
Caleb Doxsey <caleb@sendhub.com>
2012-11-13 08:11:51 -0800

    change name

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ __author__ = 'Dustin Whittle <dustin@yahoo-inc.com>'
 from distutils.core import setup
 
 setup(
-    name='yos_social_sdk',
-    version='0.0.4',
+    name='yos-social-sdk',
+    version='0.0.5',
     description='Python client library for Yahoo! Social + Data REST APIs',
     author='Dustin Whittle',
     author_email='dustin@yahoo-inc.com',
